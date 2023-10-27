8278ed59b10f84c590c6c7d50e3034c45712ad0f
sarahOmar <117687177+codeQueen211@users.noreply.github.com>
2023-10-27 16:04:57 +0300

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,32 @@
+import turtle;
+
+tim= turtle.Turtle()
+tim.color('pink')
+tim.shape("turtle")
+tim.pensize(5)
+tim.circle(100)
+
+
+tim.color('blue')
+tim.circle(90)
+
+tim.color('green')
+tim.circle(80)
+
+tim.color('purple')
+tim.circle(70)
+
+tim.color('red')
+tim.circle(60)
+
+tim.color('yellow')
+tim.circle(50)
+
+tim.color('black')
+tim.circle(40)
+
+tim.color('gray')
+tim.circle(30)
+
+tim.color('orange')
+tim.circle(20)
